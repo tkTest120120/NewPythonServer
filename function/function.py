935179e1c266eba7515ec325cbe1c61f1ba31dac
huy2012 <huynn@reactplus.jp>
2021-12-30 16:15:52 +0700

    update selenium print comment

--- function/function.py
+++ function/function.py
@@ -10,14 +10,15 @@ import json
 from webdriver_manager.chrome import ChromeDriverManager
 
 def get_Link_img(url):
-    import os
-    from selenium import webdriver
     op = webdriver.ChromeOptions()
     op.binary_location = os.environ.get("GOOGLE CHROME_BIN")
     op.add_argument("--headless")
     op.add_argument("--no-sandbox")
     op.add_argument("--disable-dev-sh-usage")
     driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), chrome_options = op)
+
+
+
     # chrome_options = webdriver.ChromeOptions()
     # prefs = {
     #     "profile.managed_default_content_settings.images": 2
@@ -39,22 +40,21 @@ def get_Link_img(url):
     soup = BeautifulSoup(html, 'html.parser')
 
     sum = 0
-    # link_json = {}
     link_list = []
 
     for link in soup.find_all('img'):
         link = link.get('src')
-        print(link)
+        # print(link)
         sum += 1
-        # link_json["name"] = str(sum)
-        # link_json["link"] = str(link)
         link_list.append(link)
 
     # print(soup)
-    print(f'sum :  {sum}')
+    print(f'sum :  {sum} link ảnh')
     link_json = json.dumps(link_list)
-    print(link_json)
-
+    # print(link_json)
 
     driver.quit()
     return link_json
+
+
+get_Link_img("https://www.facebook.com/groups/697332711026460/media/photos")
