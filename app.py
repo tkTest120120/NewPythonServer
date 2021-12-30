0168f1ed8b88b1117d7af15a4714053e0e8281f8
huy2012 <huynn@reactplus.jp>
2021-12-30 15:03:25 +0700

    update selenium

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 # pip freeze > requirements.txt export library to file txt
 from flask import Flask, render_template
+from function.function import get_Link_img
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    link = get_Link_img("https://www.facebook.com/groups/697332711026460/media/photos")
+    return link
 
 
 @app.route("/salvador")
 def salvador():
     return "Hello, Salvador"
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
--- function/function.py
+++ function/function.py
@@ -3,37 +3,50 @@ import os
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
+import json
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as ec
 from webdriver_manager.chrome import ChromeDriverManager
 
-chrome_options = webdriver.ChromeOptions()
-prefs = {
-    "profile.managed_default_content_settings.images": 2
-}
-chrome_options.add_experimental_option("prefs", prefs)
+def get_Link_img(url):
+    chrome_options = webdriver.ChromeOptions()
+    prefs = {
+        "profile.managed_default_content_settings.images": 2
+    }
+    chrome_options.add_experimental_option("prefs", prefs)
 
-# driver = webdriver.Chrome('./chromedriver')
-# driver = webdriver.Chrome('./chromedriver', options=chrome_options)
-driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
+    # driver = webdriver.Chrome('./chromedriver')
+    # driver = webdriver.Chrome('./chromedriver', options=chrome_options)
+    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
 
-url = "https://www.facebook.com/groups/697332711026460/media/photos"
+    # url = "https://www.facebook.com/groups/697332711026460/media/photos"
 
-driver.get(url)
-sleep(3)
-html = driver.page_source
+    driver.get(url)
+    sleep(2)
+    html = driver.page_source
 
-# html = requests.get(url).text
+    # html = requests.get(url).text
 
-soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
 
-sum = 0
-for link in soup.find_all('img'):
-    print(link.get('src'))
-    sum += 1
+    sum = 0
+    # link_json = {}
+    link_list = []
 
-# print(soup)
-print(f'sum :  {sum}')
+    for link in soup.find_all('img'):
+        link = link.get('src')
+        print(link)
+        sum += 1
+        # link_json["name"] = str(sum)
+        # link_json["link"] = str(link)
+        link_list.append(link)
 
-driver.quit()
+    # print(soup)
+    print(f'sum :  {sum}')
+    link_json = json.dumps(link_list)
+    print(link_json)
+
+
+    driver.quit()
+    return link_json
